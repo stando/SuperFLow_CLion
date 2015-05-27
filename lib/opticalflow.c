5ab948ad34f909c22cab9d8c1120bb1627bd7615
Yijun Pan <panyi@student.ethz.ch>
2015-05-27 16:00:04 +0200

    dummy

--- lib/opticalflow.c
+++ lib/opticalflow.c
@@ -92,7 +92,7 @@ void compute_one_level(image_t *wx, image_t *wy, color_image_t *im1, color_image
         // sor_coupled_blocked_1x4(du_2, dv_2, a11, a12, a22, b1, b2, smooth_horiz, smooth_vert, params->n_solver_iteration, params->sor_omega);
 
         // blocked SOR with 2x2 mini blocks
-        //sor_coupled_blocked_2x2(du_2, dv_2, a11, a12, a22, b1, b2, smooth_horiz, smooth_vert, params->n_solver_iteration, params->sor_omega);
+        sor_coupled_blocked_2x2(du_2, dv_2, a11, a12, a22, b1, b2, smooth_horiz, smooth_vert, params->n_solver_iteration, params->sor_omega);
 
         // blocked SOR with vectorization
         //sor_coupled_blocked_2x2_vectorization(du, dv, a11, a12, a22, b1, b2, smooth_horiz, smooth_vert, params->n_solver_iteration, params->sor_omega);
